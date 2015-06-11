2b3d3d46cd49f0e28ebd5d126c82df874bc05d20
ikeda_yoshihito <ikeda_yoshihito@cyberagent.co.jp>
2015-06-12 06:46:28 +0900

    Update versions of libraries

--- sample/src/main/java/com/yqritc/recyclerviewmultipleviewtypesadapter/sample/MainActivity.java
+++ sample/src/main/java/com/yqritc/recyclerviewmultipleviewtypesadapter/sample/MainActivity.java
@@ -3,7 +3,7 @@ package com.yqritc.recyclerviewmultipleviewtypesadapter.sample;
 import com.yqritc.recyclerviewmultipleviewtypesadapter.sample.adapter.SampleEnumMapAdapter;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +23,21 @@ public class MainActivity extends ActionBarActivity {
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        adapter.setSample2Data(getSampleData());
+        List<SampleData> sampleData = getSampleData();
+        adapter.setSample2Data(sampleData);
     }
 
     private List<SampleData> getSampleData() {
         List<SampleData> dataSet = new ArrayList<>();
         for (int i = 1; i <= 4; i++) {
             SampleData data = new SampleData();
-            data.mTitle = "View Type 2";
-            data.mDrawableResId =
-                    getResources().getIdentifier("animal" + i, "drawable", getPackageName());
-            data.mContent = "This is sample data for view type 2 - " + i;
+            data.mTitle = getString(R.string.title_type2);
+            data.mDrawableResId = getResources().getIdentifier(
+                    getString(R.string.drawable_animal_name, i), "drawable", getPackageName());
+            data.mContent = getString(R.string.content_type2, i);
             dataSet.add(data);
         }
+
         return dataSet;
     }
 }
